f5b44c368db78388c8f61468474a0217ee29bf7f
RuneLind <rulind@gmail.com>
2020-05-23 12:56:43 +0200

    Add assert to SchemaToTemplateTest

--- src/test/java/no/ssb/dapla/dataset/doc/template/SchemaToTemplateTest.java
+++ src/test/java/no/ssb/dapla/dataset/doc/template/SchemaToTemplateTest.java
@@ -5,6 +5,8 @@ import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SchemaToTemplateTest {
 
     @Test
@@ -40,7 +42,37 @@ class SchemaToTemplateTest {
                                 "population");
 
         String jsonString = schemaToTemplate.generateSimpleTemplateAsJsonString();
-        System.out.println(jsonString);
+        String expected = "{\n" +
+                "  \"dataset-path\" : \"/path/to/dataset\",\n" +
+                "  \"logical-record-root\" : {\n" +
+                "    \"name\" : \"root\",\n" +
+                "    \"logicalRecords\" : [ {\n" +
+                "      \"name\" : \"person\",\n" +
+                "      \"logicalRecords\" : [ {\n" +
+                "        \"name\" : \"address\",\n" +
+                "        \"instanceVariables\" : [ {\n" +
+                "          \"name\" : \"street\",\n" +
+                "          \"description\" : \"street\"\n" +
+                "        }, {\n" +
+                "          \"name\" : \"postcode\",\n" +
+                "          \"description\" : \"postcode\"\n" +
+                "        } ]\n" +
+                "      } ],\n" +
+                "      \"instanceVariables\" : [ {\n" +
+                "        \"name\" : \"name\",\n" +
+                "        \"description\" : \"name\"\n" +
+                "      }, {\n" +
+                "        \"name\" : \"sex\",\n" +
+                "        \"description\" : \"sex\"\n" +
+                "      } ]\n" +
+                "    } ],\n" +
+                "    \"instanceVariables\" : [ {\n" +
+                "      \"name\" : \"group\",\n" +
+                "      \"description\" : \"group\"\n" +
+                "    } ]\n" +
+                "  }\n" +
+                "}";
+        assertThat(jsonString).isEqualTo(expected);
     }
 
     @Test
@@ -50,7 +82,7 @@ class SchemaToTemplateTest {
                 .fields()
                 .name("kontonummer").prop("description", "vilkårlig lang sekvens av tegn inkludert aksenter og spesielle tegn fra standardiserte tegnsett").type().stringType().noDefault()
                 .name("innskudd").prop("description", "9 sifret nummer gitt de som er registrert i Enhetsregisteret.").type().stringType().noDefault()
-                .name("gjeld").prop("description", "en sum av penger i hele kroner brukt i en kontekst. Dette kan være en transaksjon, saldo o.l."). type().optional().stringType()
+                .name("gjeld").prop("description", "en sum av penger i hele kroner brukt i en kontekst. Dette kan være en transaksjon, saldo o.l.").type().optional().stringType()
                 .endRecord();
 
         SchemaToTemplate schemaToTemplate =
@@ -66,6 +98,23 @@ class SchemaToTemplateTest {
                                 "population");
 
         String jsonString = schemaToTemplate.generateSimpleTemplateAsJsonString();
-        System.out.println(jsonString);
+        String expected = "{\n" +
+                "  \"dataset-path\" : \"/path/to/dataset\",\n" +
+                "  \"logical-record-root\" : {\n" +
+                "    \"name\" : \"konto\",\n" +
+                "    \"instanceVariables\" : [ {\n" +
+                "      \"name\" : \"kontonummer\",\n" +
+                "      \"description\" : \"vilkårlig lang sekvens av tegn inkludert aksenter og spesielle tegn fra standardiserte tegnsett\"\n" +
+                "    }, {\n" +
+                "      \"name\" : \"innskudd\",\n" +
+                "      \"description\" : \"9 sifret nummer gitt de som er registrert i Enhetsregisteret.\"\n" +
+                "    }, {\n" +
+                "      \"name\" : \"gjeld\",\n" +
+                "      \"description\" : \"en sum av penger i hele kroner brukt i en kontekst. Dette kan være en transaksjon, saldo o.l.\"\n" +
+                "    } ]\n" +
+                "  }\n" +
+                "}";
+
+        assertThat(jsonString).isEqualTo(expected);
     }
 }
